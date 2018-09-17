e57b62b29ce290dafda624e540c593784aa77f16
金加斌 <cokeknight1@163.com>
2018-09-17 21:55:46 +0800

    add

--- scripts/job.js
+++ scripts/job.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-
+const http = require('http');
+const querystring= require('querystring');
 function getData (file) {
 	const file1Path = path.resolve(__dirname, `../data/${file}.txt`)
 
@@ -14,18 +15,67 @@ function getData (file) {
 	})
 	return output
 }
+const stockData = []
+const postData = querystring.stringify({
+  'ddlShareholdingMonth': '09',
+  ddlShareholdingYear: '2018',
+  ddlShareholdingDay: 15
+});
+
+const options = {
+  hostname: 'www.hkexnews.hk',
+  port: 80,
+  path: '/sdw/search/mutualmarket_c.aspx?t=sh',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(postData)
+  }
+};
+
+const req = http.request(options, (res) => {
+  // console.log(`STATUS: ${res.statusCode}`);
+  // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  let datas = ''
+
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+  	datas += chunk
+  });
+  res.on('end', () => {
+  	const chunks = datas.split(/<tr class="row[0|1]"/g)
+    chunks.forEach((item) => {
+    	const stockName = item.match(/<td valign="top" class="arial12black">[\s\t\r]+([\w\W]{0,4})[\s\t\r]+<\/td>/)
+    	const stockAmount = item.match(/<td valign="top" nowrap="nowrap" class="arial12black" style="text-align: right;">[\s\t\r]+([\w\W]{0,20})[\s\t\r]+<\/td>/)
+    	
+    	if (stockName && stockAmount){
+    		stockData.push(stockName[1]+ ' ' + stockAmount[1].replace(/\r\n/g, ''))
+    	}
+    })
+    console.log(stockData)
+    // console.log('No more data in response.');
+  });
+});
+
+req.on('error', (e) => {
+  // console.error(`problem with request: ${e.message}`);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
 const file1 = getData('20180824')
 const file9 = getData('20180902')
 const file2 = getData('20180710')
-Object.keys(file2).forEach((item) => {
-	if (file2[item] && !file1[item]) {
-		console.log('上个月不存在的',item)
-		return;
-	}
-	if (!file2[item] || !file1[item]) return;
-	let nowData = file2[item].replace(/,/g,'')
-	let prevData = file1[item].replace(/,/g,'')
-	if ((nowData-prevData)/prevData > 1 && (nowData-prevData)/prevData < 4 ) {
-		console.log(item, nowData, prevData)
-	}
-})
+// Object.keys(file2).forEach((item) => {
+// 	if (file2[item] && !file1[item]) {
+// 		console.log('上个月不存在的',item)
+// 		return;
+// 	}
+// 	if (!file2[item] || !file1[item]) return;
+// 	let nowData = file2[item].replace(/,/g,'')
+// 	let prevData = file1[item].replace(/,/g,'')
+// 	if ((nowData-prevData)/prevData > 1 && (nowData-prevData)/prevData < 4 ) {
+// 		console.log(item, nowData, prevData)
+// 	}
+// })
